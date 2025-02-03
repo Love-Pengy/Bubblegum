85028551da1e54865d6977f6faca2d76fb754b5b
Brandon Frazier <132213413+Love-Pengy@users.noreply.github.com>
2025-02-02 19:27:50 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ source "servers.sh"
 
 UHOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 echo $UHOME
+nonRoot="sudo -u $SUDO_USER bash -c"
 install="pacman -S --noconfirm"
 
 # ####################### #
@@ -26,17 +27,14 @@ fi
 # ##### #
 
 pacman -Syu --noconfirm
-$install python-pip
 
 # install yay | requires manual password input 
 pacman -S --needed git base-devel 
-sudo -u $SUDO_USER bash -c 'git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si'
-pwd
+$nonRoot 'cd && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si'
 
 # My Preferred Folders
-mkdir -p $UHOME/Applications $UHOME/Projects $UHOME/Documents $UHOME/Videos \
+$nonRoot mkdir -p $UHOME/Applications $UHOME/Projects $UHOME/Documents $UHOME/Videos \
          $UHOME/Downloads 
-
 
 # ################ #
 # Package Download #
@@ -58,24 +56,23 @@ $install obs-studio
 
 
 ## Install Rust |  requires user input
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+$nonRoot curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Install cargo 
 $install cargo 
 
-# Install Wallust 
-cargo install wallust
-
 # nerdfonts
-curl https://api.github.com/repos/ryanoasis/nerd-fonts/tags | grep "tarball_url" | grep -Eo 'https://[^\"]*' | sed  -n '1p' | xargs wget -O - | tar -xz
-mkdir -p $UHOME/.local/share/fonts
-find ./ryanoasis-nerd-fonts-* -name '*.ttf' -exec cp {} $UHOME/.local/share/fonts \;
-rm -rf ./ryanoasis-nerd-fonts-*
+$nonRoot "\
+curl https://api.github.com/repos/ryanoasis/nerd-fonts/tags | grep "tarball_url" | grep -Eo 'https://[^\"]*' | sed  -n '1p' | xargs wget -O - | tar -xz\
+mkdir -p $UHOME/.local/share/fonts\
+find ./ryanoasis-nerd-fonts-* -name '*.ttf' -exec mv {} $UHOME/.local/share/fonts\;\
+rm -rf ./ryanoasis-nerd-fonts-*"
 
 # PxPlus font
-git clone https://github.com/Love-Pengy/PxPlus_IBM_VGA8_Nerd.git
-mv ./PxPlus_IBM_VGA8_Nerd/PxPlusIBMVGA8NerdFont-Regular.ttf $UHOME/.local/share/fonts 
-rm -rf PxPlus_IBM_VGA8_Nerd
+$nonRoot "\ 
+git clone https://github.com/Love-Pengy/PxPlus_IBM_VGA8_Nerd.git\
+mv ./PxPlus_IBM_VGA8_Nerd/PxPlusIBMVGA8NerdFont-Regular.ttf $UHOME/.local/share/fonts\ 
+rm -rf PxPlus_IBM_VGA8_Nerd"
 
 # obsidian
 flatpak install -y md.obsidian.Obsidian/x86_64/stable
@@ -85,24 +82,21 @@ yay -S -noconfirm vesktop
 
 # qmk
 $install qmk
-qmk setup -y -H $UHOME/Projects/qmk_firmware
+$nonRoot qmk setup -y -H $UHOME/Projects/qmk_firmware
 
-exit 1
 # ############# #
 # Configuration # 
 # ############# #
 
-# Move dotfiles to their respective places
-stow .
-
-# Allow brightnessctl to work without sudo 
-usermod -aG video ${USER} 
+# Move dotfiles to their respective places, adopts existing files then overrides them to what they should be 
+$nonRoot stow .
+$nonRoot git restore .
 
 # Make random_bg script executable
 chmod +x $UHOME/.config/sway/random_bg
 
 # install neovim servers
 for server in ${servers[@]}; do 
-  nvim --headless  +'MasonInstall $package' +qa
+  $nonRoot nvim --headless  +'MasonInstall $package' +qa
 done 
 
