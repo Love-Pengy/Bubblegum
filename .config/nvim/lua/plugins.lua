b96c131a4ef58b55cc8a695fc573cd5f50827d29
Love-Pengy <afrazierboy2001@gmail.com>
2025-02-16 23:54:10 -0500

    rm submodule

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    {
+      --BMO!!
+      {
+        "Love-Pengy/minintro.nvim",
+        opts = { color = "#63bda4" },
+        config = true,
+        lazy = false,
+        enabled = true,
+      },
+      {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+          vim.fn["mkdp#util#install"]()
+        end,
+      },
+
+      --automatic highlight disabling when done with search
+      "romainl/vim-cool",
+
+      --colorscheme maker
+      {
+        "rktjmp/lush.nvim",
+        dependencies = {
+          --colorscheme exporter
+          "rktjmp/shipwright.nvim",
+        },
+        cmd = "Lushify",
+        enabled = false,
+      },
+
+    },
+  },
+}
